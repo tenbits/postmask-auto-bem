78e2e282863c5d1729a7e3bf521b3620cf911e9f
Alex Kit <source.kit@gmail.com>
2017-11-24 10:15:29 +0100

    fix (test)

--- test/config.js
+++ test/config.js
@@ -1,14 +1,5 @@
-module.exports = {
+module.exports = {	
 	suites: {
-		dom: {
-			exec: 'dom',
-			env: [
-				'/node_modules/babel-standalone/babel.min.js',
-				'/node_modules/maskjs/lib/mask.js::mask',
-				'/lib/runtime.js'
-			],
-			tests: 'test/dom.test'
-		},
 		node: {
 			exec: 'node',
 			env: [
